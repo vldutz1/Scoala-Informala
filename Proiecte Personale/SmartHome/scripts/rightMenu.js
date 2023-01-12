7e00aac8a31e0fcd592391e1992884955363bc88
Vlad Chereches <vladchereches@yahoo.com>
2023-01-13 00:38:06 +0200

    up

--- Proiecte Personale/SmartHome/scripts/rightMenu.js
+++ Proiecte Personale/SmartHome/scripts/rightMenu.js
@@ -1,5 +1,5 @@
 function openRightMenu() {
-    document.getElementById("right-menu").style.width = "30%";
+    document.getElementById("right-menu").style.width = "20em";
     document.getElementById("right-menu-open-btn").classList.toggle("hidden");
     document.getElementById("right-menu-close-btn").classList.toggle("hidden");
 }
